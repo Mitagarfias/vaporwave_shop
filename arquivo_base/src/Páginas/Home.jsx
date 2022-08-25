a419509587f5f2dbd9bfb17cba5e5c14e6580294
BelchiorLikesWhales <106777905+BelchiorLikesWhales@users.noreply.github.com>
2022-08-25 14:39:34 -0400

    Add files via upload

---
+++ arquivo_base/src/Páginas/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'
+
+export default function Home() {
+    return (
+        <div className='container'>
+            <div className='mainbox'>
+                <div className='appexe'>
+                    <p className='frase'>
+                        Welcome.exe
+                    </p>
+
+                </div>
+
+                <div className='window'>
+
+
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
