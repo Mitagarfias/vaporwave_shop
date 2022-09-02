f732ae94f30522584e7259a2bbc93d8e5f24e6f4
BelchiorLikesWhales <106777905+BelchiorLikesWhales@users.noreply.github.com>
2022-09-02 13:50:39 -0400

    Add files via upload

---
+++ src/Páginas/RPG/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Rpg(){
+    return(
+        <div>
+            <p>Página de rpg</p>
+        </div>
+    )
+}
